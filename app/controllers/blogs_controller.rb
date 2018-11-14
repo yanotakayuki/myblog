39e57e87bd197170f0b8b002dc0c411b1d5f6ea6
矢野高惟 <yanot3151@gmail.com>
2018-11-14 22:56:50 +0900

    create be-displayed

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,25 @@
 class BlogsController < ApplicationController
-  def index
 
+  def index
+    @blogs = Blog.all.order("created_at DESC")
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+    if @blog.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'blogs#index'
+  get  'blogs'     => 'blogs#index'
+  get  'blogs/new' => 'blogs#new'
+  post 'blogs'     => 'blogs#create'
 end
